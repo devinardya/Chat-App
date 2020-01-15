569cfddee11403f264e56bc62b30fa7220b778af
Devina <devinardya@gmail.com>
2020-01-15 10:31:11 +0100

    delete counter

--- src/App.js
+++ src/App.js
@@ -8,29 +8,20 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {status : false, 
-                  username: "",
-                  counter: 0,
-                 
+                  username: "",              
                   }
     this.onPageChange = this.onPageChange.bind(this);
     this.onTextInputChange = this.onTextInputChange.bind(this);
-    this.onChangeCounter = this.onChangeCounter.bind(this);
   }
 
   onPageChange(status) {
     this.setState({status: status})
-  
-    this.setState({counter: 0})
   }
 
   onTextInputChange(value, count) {
     //this.setState({counter: value.length})
     this.setState({username: value});
-    this.setState({counter: count});
-  }
-
-  onChangeCounter(count){
-    this.setState({counter: count})
+    
   }
   
   render(){
@@ -38,7 +29,7 @@ class App extends React.Component {
     let renderPage;
     //console.log(this.state.status)
     if (!this.state.status){
-        renderPage = <Login onSubmit={this.onPageChange} onChange={this.onTextInputChange} counter={this.state.counter} username={this.state.username}/>
+        renderPage = <Login onSubmit={this.onPageChange} onChange={this.onTextInputChange} username={this.state.username}/>
     } else {
         renderPage = <Chatbox username={this.state.username} onCloseChat={this.onPageChange}/>
     }
